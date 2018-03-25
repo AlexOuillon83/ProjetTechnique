09196127d3bec11fe3adcda283ace5b62c458d72
A <alex.ouillon@outlook.fr>
2018-03-25 12:14:28 +0200

    Initial commit

---
+++ app/src/main/java/projetechnique/alex/isen/fr/projettechnique/MainActivity.java
@@ -0,0 +1,13 @@
+package projetechnique.alex.isen.fr.projettechnique;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
